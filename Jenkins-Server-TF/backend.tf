c8f6792205cadbf2c074ce70a5214a2177cf3e95
vinodkumar <mvk4459@gmail.com>
2025-10-25 14:36:30 +0530

    only backend

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "aws-tetriis-buc"
-    region         = "up-south-1"
+    region         = "ap-south-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "locking-s3"
     encrypt        = true
