faf590d7d8c76cdaa69f48a4df70090adf3c2442
vinodkumar <mvk4459@gmail.com>
2025-10-25 14:58:05 +0530

    region change

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "aws-tetriis-buc"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     dynamodb_table = "locking-s3"
     encrypt        = true
